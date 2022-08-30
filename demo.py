1bc37276b3bae5b796e9320de097372aa70932c1
zhaoqichao <zhaoqichao@t3caic.com>
2022-08-30 18:43:29 +0800

    inference demo

--- demo.py
+++ demo.py
@@ -15,7 +15,7 @@ from utils.utils import \
 
 def make_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='weights/yolopv2.pt', help='model.pt path(s)')
+    parser.add_argument('--weights', nargs='+', type=str, default='data/weights/yolopv2.pt', help='model.pt path(s)')
     parser.add_argument('--source', type=str, default='data/example.avi', help='source')  # file/folder, 0 for webcam
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.3, help='object confidence threshold')
